9a987ee05574f0b1c36358faba554f26ff69c9a4
Mareks Robalds <mareksrobalds@Marekss-MBP.localdomain>
2023-02-06 12:01:00 +0200

    Do homework

---
+++ src/Lesson9_Homework/HomeworkTask.java
@@ -0,0 +1,12 @@
+package Lesson9_Homework;
+
+/*
+Have a user, user has options to save shapes, triangle, circle, square. Should have validation.
+Input side lengths. Use exceptions. User can retrieve all stored shapes.
+User can calculate total perimeters and areas of all shapes.
+Create tests.
+*Choose which shapes to calculate.
+
+ */
+public class HomeworkTask {
+}
